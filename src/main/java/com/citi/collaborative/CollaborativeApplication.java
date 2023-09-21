be85b738166c265095d61b79f426c1ff89324323
keagancoder <keaganwang@163.com>
2023-09-21 13:45:51 +0800

    init project

---
+++ src/main/java/com/citi/collaborative/CollaborativeApplication.java
@@ -0,0 +1,14 @@
+package com.citi.collaborative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeApplication.class, args);
+	}
+
+}
+
